45b866ad429284efd2e17430907ad1b237fa3dde
saikrishna <ss682@njit.edu>
2023-07-13 17:34:08 -0400

    added fields to customer as well as jpa repository

--- Customer/src/main/java/com/blackhat/model/Customer.java
+++ Customer/src/main/java/com/blackhat/model/Customer.java
@@ -1,4 +1,23 @@
 package com.blackhat.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
 }
--- Customer/src/main/java/com/blackhat/repository/CustomerRepository.java
+++ Customer/src/main/java/com/blackhat/repository/CustomerRepository.java
@@ -1,4 +1,11 @@
 package com.blackhat.repository;
 
-public class CustomerRepository {
+import com.blackhat.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
